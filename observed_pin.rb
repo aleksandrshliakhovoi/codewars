7e4aa7274a215fb31b1fa0f4a6cbd730589efcf9
Oleksandr Shliakhovyi <aleksandrshliakhovoi@gmail.com>
2021-11-21 13:21:15 +0200

    add observed pin

--- observed_pin.rb
+++ observed_pin.rb
@@ -14,26 +14,15 @@
 def get_pins(observed)
   finish_arr = []
 
-  all_digits_arr = current_arr_seq(observed).map
+  all_digits_arr = current_arr_seq(observed)
 
   if all_digits_arr.size == 1
-    return all_digits_arr
-  elsif all_digits_arr.size <= 8 && finish_arr.size <= all_digits_arr.size 
-    var_numbers = all_digits_arr.sample(all_digits_arr.size).join(",")
-    finish_arr <<  var_numbers
+    return all_digits_arr.map!(&:to_s)
+  else
+    z = all_digits_arr.map(&:to_s).combination(observed.size).to_a if observed.size <= 8
+    z.each { |seq| finish_arr << seq.join('') }
   end
-
-  all_digits_arr.each do |num|
-
-  end
-  var_numbers = all_digits_arr.sample(all_digits_arr.size).join(",")
-  finish_arr <<  var_numbers
-
-  while test
-    
-  end
-
-  finish_arr
+  finish_arr.uniq.sort
 end
 
 def digit_sequences
@@ -54,35 +43,126 @@ end
 def current_arr_seq(seq)
   arr_from_seq = seq.split("").map(&:to_i)
   
-  hash_seq_from_arg = []
+  arr_seq_from_arg = []
   
   arr_from_seq.each do |seq_digit|
     case seq_digit
     when 0
-      hash_seq_from_arg << digit_sequences[:zero]
+      arr_seq_from_arg << digit_sequences[:zero]
     when 1
-      hash_seq_from_arg << digit_sequences[:one]
+      arr_seq_from_arg << digit_sequences[:one]
     when 2
-      hash_seq_from_arg << digit_sequences[:two]
+      arr_seq_from_arg << digit_sequences[:two]
     when 3
-      hash_seq_from_arg << digit_sequences[:three]
+      arr_seq_from_arg << digit_sequences[:three]
     when 4
-      hash_seq_from_arg << digit_sequences[:four]
+      arr_seq_from_arg << digit_sequences[:four]
     when 5
-      hash_seq_from_arg << digit_sequences[:five]
+      arr_seq_from_arg << digit_sequences[:five]
     when 6
-      hash_seq_from_arg << digit_sequences[:six]
+      arr_seq_from_arg << digit_sequences[:six]
     when 7
-      hash_seq_from_arg << digit_sequences[:seven]
+      arr_seq_from_arg << digit_sequences[:seven]
     when 8
-      hash_seq_from_arg << digit_sequences[:eight]
+      arr_seq_from_arg << digit_sequences[:eight]
     when 9
-      hash_seq_from_arg << digit_sequences[:nine]
+      arr_seq_from_arg << digit_sequences[:nine]
     end 
   end
 
-  hash_seq_from_arg.flatten
+  # get all numbers from income sequence in array structure
+  arr_seq_from_arg.flatten
 end
 
 #pp digit_sequences[:zero]
-pp current_arr_seq('912345')
+# pp current_arr_seq('912345')
+
+# pp current_arr_seq('11')
+
+p get_pins('11')
+p get_pins('369')
+
+# "three": [3, 2, 6],
+# "six":   [6, 3, 5, 9],
+# "nine":  [9, 8, 6]
+
+
+
+# RIGTH CASES
+
+  # def get_pins(observed)
+  #   mapping = {
+  #     "1" => %w(1 2 4), 
+  #     "2" => %w(1 2 3 5),
+  #     "3" => %w(2 3 6), 
+  #     "4" => %w(1 4 5 7), 
+  #     "5" => %w(2 4 5 6 8), 
+  #     "6" => %w(3 5 6 9), 
+  #     "7" => %w(4 7 8), 
+  #     "8" => %w(5 7 8 9 0), 
+  #     "9" => %w(6 8 9), 
+  #     "0" => %w(8 0)
+  #   }
+  #   observed.split(//).map {|key| mapping[key] }.reduce {|m, x| m.product(x).map(&:join) }
+  # end
+
+
+  # def get_pins(observed)
+  #   observed.chars.map(&{
+  #     '1' => [[1],[2],[4]],     '2' => [[1],[2],[3],[5]],     '3' => [[2],[3],[6]],
+  #     '4' => [[1],[4],[5],[7]], '5' => [[2],[4],[5],[6],[8]], '6' => [[3],[5],[6],[9]],
+  #     '7' => [[4],[7],[8]],     '8' => [[5],[7],[8],[9],[0]], '9' => [[6],[8],[9]],
+  #     '0' => [[0],[8]],
+  #   }).reduce(&:product).map(&:join)
+  # end
+
+
+  # def get_pins(observed)
+  #   nears = ['08 ', '124 ', '2135 ', '326 ', '4157 ', '52468 ', '6359 ', '748 ', '85790 ', '968 ']
+  #   observed = observed.gsub(/(\d)/){nears[$1.to_i]}.split.map{|s| s = s.chars.to_a}
+  #   observed.first.product(*observed.drop(1)).map{|x| x.join}
+  # end
+
+
+  # require 'matrix'
+
+  # KEYPAD = Matrix[
+  #   ["1", "2", "3"],
+  #   ["4", "5", "6"],
+  #   ["7", "8", "9"],
+  #   [nil, "0", nil]
+  # ]
+
+  # def get_pins(observed)
+  #   combos = observed.chars.map { |char|
+  #     row, col = KEYPAD.index(char)
+
+  #   [[row, col]]
+  #       .concat([row].product([col - 1, col + 1].reject { |i| i < 0 }))
+  #       .concat([row - 1, row + 1].reject { |i| i < 0 }.product([col]))
+  #       .map { |index| KEYPAD.element(*index) }
+  #       .compact
+  #   }
+
+  #   combos.shift.product(*combos).map(&:join)
+  # end
+
+  # DIGIT_VARIATIONS = {
+  #   '0' => %w(0 8),
+  #   '1' => %w(1 2 4),
+  #   '2' => %w(1 2 3 5),
+  #   '3' => %w(2 3 6),
+  #   '4' => %w(1 4 5 7),
+  #   '5' => %w(2 4 5 6 8),
+  #   '6' => %w(3 5 6 9),
+  #   '7' => %w(4 7 8),
+  #   '8' => %w(0 5 7 8 9),
+  #   '9' => %w(6 8 9)
+  # }
+  
+  # def get_pins(observed)
+  #   digit_sets = observed.chars.map{ |d| DIGIT_VARIATIONS[d] }
+  #   digit_sets.inject(['']) do |variations, digit_set|
+  #     variations = variations.map{ |v| digit_set.map {|d| "#{v}#{d}"} }.flatten
+  #   end
+  # end
